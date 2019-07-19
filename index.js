486e091040e15418afc2e076d5726c90164be254
Ben Parisot <me@benparisot.com>
2019-07-19 11:56:47 -0700

    write factorial function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('Wired up');
+//countdown practice
 function countdown(i) {
     if(i === 0) {
         console.log('done!');
@@ -8,4 +8,15 @@ function countdown(i) {
     countdown(i - 1);
 }
 
-countdown(50);
+// countdown(50);
+
+
+//factorial practice
+
+function factorial(num) {
+    if(num < 0) return;
+    if(num === 0) return 1;
+    return num * factorial(num - 1);
+}
+
+console.log(factorial(4));
