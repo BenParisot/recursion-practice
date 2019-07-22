02c4f7de5210f7c7739f3284b6b54ca6e8b5f975
Ben Parisot <me@benparisot.com>
2019-07-22 08:00:54 -0700

    write countList function

--- index.js
+++ index.js
@@ -19,4 +19,13 @@ function factorial(num) {
     return num * factorial(num - 1);
 }
 
-console.log(factorial(4));
+// console.log(factorial(4));
+
+
+function countList(arr) {
+    if(arr.length === 0) return 0;
+    return 1 + countList(arr.slice(1));
+}
+
+const arr1 = [2, 3, 1, 4, 5, 6, 4, 2, 3, 4];
+console.log(countList(arr1));
